9479dd535bd18b9e6eae52fb9d3368d95be391e3
thiagomayrink <thiagoscmpaiva@gmail.com>
2024-02-04 19:06:56 -0300

    refactor: cpf validation message

--- src/main/java/com/boardcamp/api/dtos/CustomerDTO.java
+++ src/main/java/com/boardcamp/api/dtos/CustomerDTO.java
@@ -10,7 +10,7 @@ public class CustomerDTO {
     @Size(min = 2)
     private String name;
 
-    @Size(min = 11, max = 11, message = "CPF must be only numbers")
+    @Size(min = 11, max = 11, message = "CPF must be only numbers and max 11 digits")
     @NotBlank(message = "CPF is mandatory and must be valid")
     private String cpf;
 }
